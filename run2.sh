e31b39dc35c43cb179132eeb5ace9776c621cecb
Sen Liu <cnliusen@gmail.com>
2019-09-25 08:22:16 -0700

    run scripu

--- run2.sh
+++ run2.sh
@@ -28,10 +28,10 @@
 keras_retinanet/bin/train.py \
     --freeze-backbone --random-transform \
     --weights ../data/via-samples-10/resnet50_coco_best_v2.1.0.h5 \
-    --batch-size 8 --steps 10 --epochs 10 \
+    --batch-size 8 --steps 11437 --epochs 20 \
     --gpu 0,1 \
     --multi-gpu 2 \
     --multi-gpu-force \
     via \
-    '/home/senliu/sagemaker-retina/data/via-samples-10/00ee24a8-0f27-44af-a204-46aeaed095e8-verified.json' \
-    '/home/senliu/sagemaker-retina/data/via-samples-10/01a883df-21f3-44cf-9225-164f4c16e41a-verified.json'
+    '/home/senliu/Datasets/WIDER/annotation/train_via_no_filter.json' \
+    '/home/senliu/Datasets/WIDER/annotation/val_via.json'
